ccb7854dd8a9aa44f1c01f3a94d596b891f6336a
Talib Hussain <talib001hussain@gmail.com>
2022-11-24 17:32:10 +0500

    Added "Task" Domain Model

--- src/main/java/com/talib/tasks/domain/Task.java
+++ src/main/java/com/talib/tasks/domain/Task.java
@@ -1,7 +1,37 @@
 package com.talib.tasks.domain;
 
+import lombok.*;
+import org.hibernate.Hibernate;
+
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 public class Task {
+    @Id
+    private Long id;
+    private String name;
+    private LocalDate date;
+    private Boolean completed;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Task task = (Task) o;
+        return id != null && Objects.equals(id, task.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
